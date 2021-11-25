09aa6e80a697d38bd42d0f11994613a67452b5fd
ianhudson87 <ihudson@wustl.edu>
2021-11-24 22:17:01 -0600

    converted tree to execution dag

--- TreeGen.py
+++ TreeGen.py
@@ -1,6 +1,7 @@
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
+from Node import Node
 
 
 class TreeNode:
@@ -45,5 +46,57 @@ class BinomialTree:
     def graph(self):
         self.root.graph()
 
-bt = BinomialTree(5,.18)
+
+def generate_dag(tree_root):
+    start = Node() # dag root
+    end = Node([start])
+    start.children = [end]
+    
+    pfor(tree_root, start)
+
+    return start
+
+def pfor(tree_node, dag_node):
+    # generates the parallel_for "triangle" dag for the tree node based on how many children it has (how many iterations the parallel for loop should run for)
+    # tree_node is the node in the tree that we want to generate the p_for dag for
+    # dag_node is the node that we want the root of the parallel_for
+
+    num_iterations = len(tree_node.children)
+    leaves = pfor_helper(dag_node, 0, num_iterations-1) # nodes in execution dag that are responsible for execution (leaves of the parallel_for "triangle" dag)
+    
+    if len(leaves) != num_iterations:
+        assert ValueError("should be same number of leaves as there are iterations")
+
+    for i in range(num_iterations):
+        pfor(tree_node.children[i], leaves[i])
+
+def pfor_helper(node, min, max):
+    # node: node in dag in the pfor tree
+    # min: smallest iteration in for loop this node is responsible for
+    # min: smallest iteration in for loop this node is responsible for
+    # return: leaf nodes of the subdag from node
+    if min >= max:
+        # base case
+        return [node]
+    else:
+        mid = int((min+max)/2)
+        left_child, right_child = node.be_a_spawn()
+        left_leaves = pfor_helper(left_child, min, mid)
+        right_leaves = pfor_helper(right_child, mid+1, max)
+        return left_leaves + right_leaves
+
+
+# i=20
+# while True:
+#     print(i)
+#     random.seed(i)
+#     bt = BinomialTree(5,.18)
+#     bt.graph()
+#     i+=1
+
+random.seed(55)
+bt = BinomialTree(3,.18)
 bt.graph()
+
+dag = generate_dag(bt.root)
+dag.graph()
