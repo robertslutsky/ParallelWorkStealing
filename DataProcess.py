cf371b7985ba5f6658153511bbd5164cd99aba66
ianhudson87 <ihudson@wustl.edu>
2021-12-14 17:54:10 -0600

    labeling

--- DataProcess.py
+++ DataProcess.py
@@ -13,8 +13,8 @@ for cluster in df["clusters"].unique():
         data2 = data1[mask]
         figure = plt.figure()
         axis = figure.add_subplot()
-        axis.set_title(f"Steps to Explore {tree} vs Num Processors for {cluster} Clusters")
-        axis.set_ylabel("Steps")
+        axis.set_title(f"Active Processors When Exploring {tree} vs Num Processors for {cluster} Clusters")
+        axis.set_ylabel("Average Fraction of Active Processors")
         axis.set_xlabel("Number of Processors")
         axis.set_xticks([4,8,16,32,64])
         axis.grid()
@@ -23,9 +23,10 @@ for cluster in df["clusters"].unique():
             data3 = data2[mask]
             for steal_half in data3["steal_half"].unique():
                 mask = data3["steal_half"]==steal_half
-                print(mask)
+                # print(mask)
                 data = data3[mask]
-                axis.plot(data["processors"], data["avg_frac_active"], label=method + str(steal_half), linestyle='--', marker='o')
+                h = "(half)" if steal_half else "(one)"
+                axis.plot(data["processors"], data["avg_frac_active"], label=method + h, linestyle=None, marker='o')
                 axis.legend()
         # handles, labels = axis.get_legend_handles_labels()
         # print(handles, labels)
